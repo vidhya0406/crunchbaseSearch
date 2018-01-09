55af88f062ee1a2daa7a01311c156c2f4a56dbd5
Vidhya Ranganathan <contact@vidhyaranganathan.com>
2018-01-09 05:46:15 +0000

    stringify tech

--- routes/user.js
+++ routes/user.js
@@ -51,6 +51,7 @@ router.get('/', ensureLoggedIn, function(req, res, next) {
                   for (var i=0; i<data.length; i++){
                     var split_data = data[i].category_list.split("|");
                     data[i].category_list = JSON.stringify(split_data);
+                    data[i].technology = JSON.stringify(data[i].technology);
                   }
                   res.render('user', {user: req.user,btc: data});
                 }
