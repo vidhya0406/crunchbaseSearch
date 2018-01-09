ceb59ed84088e5e0affbd578f3c77ded2e5294d5
Vidhya Ranganathan <contact@vidhyaranganathan.com>
2018-01-09 06:27:19 +0000

    remove converting to array

--- routes/user.js
+++ routes/user.js
@@ -47,13 +47,7 @@ router.get('/', ensureLoggedIn, function(req, res, next) {
                   console.log(err);
                   console.log("Failed to fetch the collection: ", collection);
                 }
-                else {
-                  for (var i=0; i<data.length; i++){
-                    var split_data = data[i].category_list.split("|");
-                    data[i].category_list = JSON.stringify(split_data);
-                    data[i].technology = JSON.stringify(data[i].technology);
-                  }
-                  res.render('user', {user: req.user,btc: data});
+                res.render('user', {user: req.user,btc: data});
                 }
               });
             });
